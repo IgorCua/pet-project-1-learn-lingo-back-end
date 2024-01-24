71a491ed1b1a673a5e447116b72c0c52f1d0b54c
IhorC. <igorchua@gmail.com>
2024-01-24 15:57:45 -0600

    firebase-admin installed, db settings added

---
+++ firebase.js
@@ -0,0 +1,33 @@
+const {initializeApp, cert} = require('firebase-admin/app');
+const {getFirestore} = require('firebase-admin/firestore');
+// const admin = require("firebase-admin");
+const serviceAccount = require("./credentials.json");
+
+// const serviceAccount = "";
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBISxAP7xjjd1Jfw9seOevkIEvQ-PROauI",
+//     authDomain: "pet-project-1-learnlingo-21c4c.firebaseapp.com",
+//     databaseURL: "https://pet-project-1-learnlingo-21c4c-default-rtdb.firebaseio.com",
+//     projectId: "pet-project-1-learnlingo-21c4c",
+//     storageBucket: "pet-project-1-learnlingo-21c4c.appspot.com",
+//     messagingSenderId: "925544374892",
+//     appId: "1:925544374892:web:ddb296f7082d0b413c2e49",
+//     measurementId: "G-LZ9RLMS0Y4"
+// };
+
+// var admin = require("firebase-admin");
+
+// var serviceAccount = require("path/to/serviceAccountKey.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://pet-project-1-learnlingo-21c4c-default-rtdb.firebaseio.com"
+// });
+
+initializeApp({
+    credential: cert(serviceAccount)
+});
+
+const fireDb = getFirestore();
+
+module.exports = { fireDb };
