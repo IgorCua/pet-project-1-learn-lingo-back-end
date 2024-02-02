ea6e343ec3e1b589dce1c46ce1a7b0d2f88f06be
IhorC. <igorchua@gmail.com>
2024-02-01 20:53:59 -0600

    returned back to firebase-admin

--- controllers/teachers-controller.js
+++ controllers/teachers-controller.js
@@ -1,58 +1,52 @@
 const { HttpError } = require('../helpers/index');
 const ctrlWrapper = require('../utils/ctrlWrapper');
-const { ref, get, child, getDatabase } = require('firebase/database');
-// const { fireDb } = require('../firebase');
-const { db, app } = require('../firebase');
+// const { 
+//   ref, 
+//   get, 
+//   child, 
+//   getDatabase, 
+//   startAt, 
+//   limitToFirst, 
+//   orderByKey, 
+//   query
+// } = require('firebase/database');
+
+const {
+  getDatabase
+} = require('firebase-admin/database');
+
+const { fireDb } = require('../firebase');
+// const { db, app } = require('../firebase');
+
+// FirebaseDatabase.getInstance(firebaseApp)
+//     .getReference("resources")
+//     .child(FSeason.key)
+//     .orderByKey()
+//     .startAt(id)
+//     .limit(size)
 
 const getTeachersList = async (req, res) => {
-    // const
-    const teachersRef = ref(db);
-    const dbRef = ref(getDatabase(app));
-    const teachersList = await get(child(dbRef, `/teachers`)).then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log(snapshot.val());
-        return snapshot.val();
-      } else {
-        console.log("No data available");
-      }
+    // const teachersRef = ref(db, '/users');
+    // const teachersList = ref(fireDb, '/teachers')
+    const doc = await fireDb.ref('/users').orderByKey().startAt('-NobcoVECbDgSVya_YoG').get().then((snapshot) => {
+      console.log(snapshot.val());
+      return snapshot.val();
     }).catch((error) => {
-      console.error(error);
+      console.log(error);
     });
-    console.log()
-    // const doc = await get(child(teachersRef, '/users').than((snapshot) => {
-    //     if(snapshot.exists()){
-    //         console.log(snapshot,val());
-    //     } else {
-    //         console.log('data is not available');
-    //     }
+    // const teachersList = await get(child(teachersRef, '/users')).then((snapshot) => {      
+    // const teachersList = await get(teachersRef).then((snapshot) => {      
+    //   return snapshot.val();  
     // }).catch((error) => {
-    //     console.log(error);
-    // }))
-    
-    // .than((snapshot) => {
-    //     if(snapshot.exists()){
-    //         console.log(snapshot,val());
-    //     } else {
-    //         console.log('data is not available');
-    //     }
-    // }).catch((error) => {
-    //     console.log(error);
-    // })
-    // console.log(doc)
-    // console.log(doc.exists());
-    // if(!doc.exists()) {
-    //     return HttpError(400)
-    // }
-    console.log()
-    // console.log(doc)
-    // res.json({
-    //     "key": "Hello world",
-    //     "teachersList": teachersList
-    // })
+    //   if (!snapshot.exists()) HttpError(404);
 
-    res.status(200).send(teachersList);
-    // res.status(200).send(doc);
+    //   return error;
+    // });
+    // console.log(doc)
+    // res.status(200).send(teachersList);
+    res.status(200).send(doc);
 }
+
 
 module.exports = {
     getTeachersList: ctrlWrapper(getTeachersList)
--- firebase.js
+++ firebase.js
@@ -1,44 +1,44 @@
-const { initializeApp } = require('firebase/app');
-const { getDatabase } = require('firebase/database');
-// const {initializeApp, cert} = require('firebase-admin/app');
-// const { getDatabase } = require('firebase-admin/database');
-const {
-    API_KEY,
-    AUTH_DOMAIN,
-    DATABASE_URL,
-    PROJECT_ID,
-    STORAGE_BUCKET,
-    MESSAGING_SENDER_ID,
-    APP_ID,
-    MEASUREMENT_ID,
-} = process.env
+// const { initializeApp } = require('firebase/app');
+// const { getDatabase } = require('firebase/database');
+const {initializeApp, cert} = require('firebase-admin/app');
+const { getDatabase } = require('firebase-admin/database');
+// const {
+//     API_KEY,
+//     AUTH_DOMAIN,
+//     DATABASE_URL,
+//     PROJECT_ID,
+//     STORAGE_BUCKET,
+//     MESSAGING_SENDER_ID,
+//     APP_ID,
+//     MEASUREMENT_ID,
+// } = process.env
 
-const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    databaseURL: DATABASE_URL,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID,
-    appId: APP_ID,
-    measurementId: MEASUREMENT_ID
-};
+// const firebaseConfig = {
+//     apiKey: API_KEY,
+//     authDomain: AUTH_DOMAIN,
+//     databaseURL: DATABASE_URL,
+//     projectId: PROJECT_ID,
+//     storageBucket: STORAGE_BUCKET,
+//     messagingSenderId: MESSAGING_SENDER_ID,
+//     appId: APP_ID,
+//     measurementId: MEASUREMENT_ID
+// };
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+// const app = initializeApp(firebaseConfig);
+// const db = getDatabase(app);
 
-module.exports = {
-    db,
-    app
-}
-// // const admin = require("firebase-admin");
-// const { serviceAccount } = require("./credentials");
+// module.exports = {
+//     db,
+//     app
+// }
+// const admin = require("firebase-admin");
+const { serviceAccount } = require("./credentials");
 
-// let app = initializeApp({
-//     credential: cert(serviceAccount),
-//     databaseURL: "https://pet-project-1-learnlingo-21c4c-default-rtdb.firebaseio.com"
-// });
+let app = initializeApp({
+    credential: cert(serviceAccount),
+    databaseURL: "https://pet-project-1-learnlingo-21c4c-default-rtdb.firebaseio.com"
+});
 
-// const fireDb = getDatabase(app);
+const fireDb = getDatabase(app);
 
-// module.exports = { fireDb };
+module.exports = { fireDb };
