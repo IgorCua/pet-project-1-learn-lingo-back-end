d8fa33621a29ef92d52de6e40643f9c9a2075d28
IhorC. <igorchua@gmail.com>
2024-02-11 12:19:35 -0600

    (fix) authenticate, logOut

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,19 +6,22 @@ const { SECRET_KEY } = process.env;
 const authenticate = async (req, _, next) => {
     const { authorization = '' } = req.headers;
     const [ bearer, token ] = authorization.split(' ');
-
+    console.log("authenticate", req.body)
     if(bearer !== "Bearer"){
         next(HttpError(401, 'Unauthorized'));
     }
 
     try{
-        const { id } = jwt.verify(token, SECRET_KEY);
+        const { email, id } = jwt.verify(token, SECRET_KEY);
+        // const verify = jwt.verify(token, SECRET_KEY);
         const user = (await fireDb.ref('/users').child(id).get()).val();
         // const userObj = user.val();
+        // console.log('authenticate', user)
+        // console.log('authenticate', verify)
+        if(!user || !token || email !== user.email) next(HttpError(401, 'Unauthorized'))
+        // if(!user || !user.token || token !== user.token) next(HttpError(401, 'Unauthorized'))
 
-        if(!user || !user.token || token !== user.token) next(HttpError(401, 'Unauthorized'))
-
-        req.user = user;
+        // req.user = user;
         
         next();
     } catch (error) {
